51bf4291bdb7edc542b68b7084cfe1653fa9998b
Bnt-dev <bberanger.pro@gmail.com>
2023-03-28 16:26:09 +0200

    React quest passing props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import MyTitle from "./components/MyTitle";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+      imgSrc: 'src/assets/Mew.jfif'
+    },
+  ];
+
   return (
     <div>
       <MyTitle />
-      <PokemonCard />
+      <PokemonCard pokemon={pokemonList[1].name} imgPokemon={pokemonList[1].imgSrc} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,8 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-function pokemonCard() {
-  let pokemon = pokemonList[0];
-  if (pokemon.imgSrc != null) { return <figure><img src={pokemon.imgSrc} alt={pokemon.name} /><figcaption>{pokemon.name}</figcaption></figure> }
-  else { return <figure><p>???</p><figcaption>{pokemon.name}</figcaption></figure> }
-  ;
+const pokemonCard = ({ pokemon, imgPokemon }) => {
+  return <div>
+    <div>{pokemon}</div>
+    <img src={imgPokemon} alt={pokemon} />
+  </div>
 }
 
 
