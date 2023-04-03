4884e94e969d313b3b4c52ca7681666239acd30d
Bnt-dev <bberanger.pro@gmail.com>
2023-04-03 11:15:36 +0200

    React quest 8 Repeat a component with map

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,21 @@
 const NavBar = ({ pokemonIndex, setPokemonIndex, pokemonList }) => {
-  const nextClick = () => {
-    if (pokemonIndex < pokemonList.length - 1 && pokemonIndex != 2) { setPokemonIndex(pokemonIndex + 1); console.log(pokemonIndex); }
-    else if (pokemonIndex < pokemonList.length - 1 && pokemonIndex === 2) {
-      setPokemonIndex(pokemonIndex + 1), alert("Pika pika !!!"); console.log(pokemonIndex);
-    }
-  }
 
-  const previousClick = () => {
-    if (pokemonIndex > 0 && pokemonIndex != 4) {
-      setPokemonIndex(pokemonIndex - 1); console.log(pokemonIndex)
-    }
-    else if (pokemonIndex > 0) { alert("Pika pika !!!"), setPokemonIndex(pokemonIndex - 1); console.log(pokemonIndex) }
-  }
 
-  return (<div className="button">
-    <button onClick={previousClick} >Précédent</button>
-    <button onClick={nextClick} >Suivant</button>
-  </div>)
+  const handleClick = (index) => (
+    setPokemonIndex(index)
+  )
+    ;
+
+  return (<div>
+    {pokemonList.map((pokemon, index) =>
+      <button key={pokemon.name} onClick={() => handleClick(index)}>{pokemon.name}</button>
+    )
+
+    }
+  </div>
+  )
+
+
 }
 
 
